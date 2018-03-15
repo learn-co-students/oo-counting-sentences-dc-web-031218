cc7f8415cab9fdde16d76e37f910ab16efbba4c5
rpyoungdale <youngdale95@gmail.com>
2018-03-14 20:30:11 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
 require 'pry'
 
+
+
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[?!.] /).count
   end
 end
